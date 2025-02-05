7a3a500b0de8ea4e9e152385a7e74aa0e48795c0
Hansanghyun-github <han41562@gmail.com>
2025-02-06 08:37:11 +0900

    study golang - defer

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func TestDefer(str *string) {
+	// add world to str
+	defer func() {
+		*str += " World!"
+	}()
+
+	// add hello to str
+	*str += "Hello"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	str := ""
+	TestDefer(&str)
+	// validate str using assert
+	if str != "Hello World!" {
+		panic("Test failed")
+	}
+	fmt.Print("Test passed with: ", str)
 }
